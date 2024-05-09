a79a49b9acef3cde7c7099287d91015be086a4a0
sealgair <hunter.jay.k@gmail.com>
2024-05-09 20:06:13 +1000

    Added syntax formatting packages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,20 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import rehypePrettyCode from "rehype-pretty-code";
+
+/** @type {import("rehype-pretty-code").Options} */
+const options = {
+  // See Options section
+};
 
 export default defineConfig({
   site: "https://sealgair.dev",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex, [rehypePrettyCode, options]]
+  }
 });
